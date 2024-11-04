1d38672b471e9067f028b2f964ae3bbe9d975c70
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-04 11:23:31 +0300

    сделано дз

--- src/Main.java
+++ src/Main.java
@@ -78,3 +78,20 @@ public class Main {
         System.out.println("В школе, где " + totalClasses + " классов, нужно" + totalWhiteCans + " банок белой краски и " + totalBrownCans + " банок коричневой краски");
     }
 }
+
+//6 задача
+public class Main {
+    public static void main(String[] args) {
+        int bananasCount = 5;
+        int bananasWeight = 80;
+        int milkVolume = 200;
+        double iceCreamWeight = 2 * 100;
+        int eggsCount = 4;
+        int eggsWeight = 70;
+
+        int totalWeight = bananasCount * bananasWeight + milkVolume * 105 / 100 + (int) iceCreamWeight + eggsCount * eggsWeight;
+        double totalWeightInKg = totalWeight / 1000.0;
+
+        System.out.printf("Общий вес завтрака: %d грамм (%.2f кг)", totalWeight, totalWeightInKg);
+    }
+}
