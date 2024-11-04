d9bf5d4da1148902d9ff0dbc7ecda9f275fdc11a
gulyaeva-m-skyeng <marucosmos0924@gmail.com>
2024-11-04 11:14:20 +0300

    сделано дз

--- src/Main.java
+++ src/Main.java
@@ -62,3 +62,19 @@ public class Main {
         System.out.println("За месяц машина произвела " + bottlesInMonth + " штук бутылок");
     }
 }
+
+//5 задача
+public class Main {
+    public static void main(String[] args) {
+        int totalCans = 120;
+        int whiteCansPerClass = 2;
+        int brownCansPerClass = 4;
+
+
+        int totalClasses = totalCans / (whiteCansPerClass + brownCansPerClass);
+        int totalWhiteCans = totalClasses * whiteCansPerClass;
+        int totalBrownCans = totalClasses * brownCansPerClass;
+
+        System.out.println("В школе, где " + totalClasses + " классов, нужно" + totalWhiteCans + " банок белой краски и " + totalBrownCans + " банок коричневой краски");
+    }
+}
